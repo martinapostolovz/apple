6e1263d44ee32cd254dbb1ab1a7bc89fc35cb086
martinapostolov1 <prgaming633@gmail.com>
2024-03-23 20:57:38 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,45 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className='header'>
+        <div className='logo'></div>
+        <div className='products'>
+          <button id='new' className='button-header'>New</button>
+          <button id='iphone' className='button-header'>Iphone</button>
+          <button id='macbook' className='button-header'>Macbook</button>
+          <button id='watch' className='button-header'>Watch</button>
+          <button id='ipad' className='button-header'>Ipad</button>
+          <button id='vision' className='button-header'>Vision</button>
+        </div>
+        <div className='products-mobile-background'>
+          <div className='menu-logo'>
+            <div className='line'></div>
+            <div className='line'></div>
+            <div className='line'></div>
+          </div>
+          <div className='mobile-products'>
+            <button id='new-mob' className='button-header-mob'>New</button>
+            <button id='iphone-mob' className='button-header-mob'>Iphone</button>
+            <button id='macbook-mob' className='button-header-mob'>Macbook</button>
+            <button id='watch-mob' className='button-header-mob'>Watch</button>
+            <button id='ipad-mob' className='button-header-mob'>Ipad</button>
+            <button id='vision-mob' className='button-header-mob'>Vision</button>
+          </div>
+        </div>
+
+      </header>
+      <main>
+        <div className='vision-pro'>
+          <div className='image-background'>
+            <div className='new-background-text'>New</div>
+            <img className='vision-image' src="src/public/images/apple-vision-pro.png" alt="" />
+
+          </div>
+        </div>
+      </main>
     </div>
   )
 }
