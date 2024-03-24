26a1bb7a609b390fb84067cb8b3f8bd6751f3e65
martinapostolov1 <prgaming633@gmail.com>
2024-03-24 14:28:42 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ function App() {
           <button id='macbook' className='button-header'>Macbook</button>
           <button id='watch' className='button-header'>Watch</button>
           <button id='ipad' className='button-header'>Ipad</button>
-          <button id='vision' className='button-header'>Vision</button>
         </div>
         <div className='products-mobile-background'>
           <div className='menu-logo'>
@@ -28,7 +27,6 @@ function App() {
             <button id='macbook-mob' className='button-header-mob'>Macbook</button>
             <button id='watch-mob' className='button-header-mob'>Watch</button>
             <button id='ipad-mob' className='button-header-mob'>Ipad</button>
-            <button id='vision-mob' className='button-header-mob'>Vision</button>
           </div>
         </div>
 
@@ -37,8 +35,32 @@ function App() {
         <div className='vision-pro'>
           <div className='image-background'>
             <div className='new-background-text'>New</div>
-            <img className='vision-image' src="src/public/images/apple-vision-pro.png" alt="" />
-
+            <img className='vision-image' src={"https://www.apple.com/newsroom/images/media/introducing-apple-vision-pro/Apple-WWDC23-Vision-Pro-glass-230605.jpg.landing-big_2x.jpg"} />
+          </div>
+          <div className='iphones-flex'>
+          </div>
+        </div>
+        <br />
+        <div className='background-iphones'>
+          <div className='sec-background-iphones'>
+            <div className='iphone-background'>
+              <div className='new-background-text'>New</div>
+              <img className='iphone15' src="./images/iphone15-black.png" alt="" />
+              <br />
+              <p className='text-name-phone'>Iphone 15 </p>
+            </div>
+            <div className='iphone-background'>
+              <div className='new-background-text'>New</div>
+              <img className='iphone15' src="./images/iphone_15_pro_max.png" alt="" />
+              <br />
+              <p className='text-name-phone'>Iphone 15 PRO</p>
+            </div>
+            <div className='iphone-background'>
+              <div className='new-background-text'>New</div>
+              <img className='iphone15' src="./images/iphone_15_pro_max.png" />
+              <br />
+              <p className='text-name-phone'>Iphone 15 PRO MAX</p>
+            </div>
           </div>
         </div>
       </main>
